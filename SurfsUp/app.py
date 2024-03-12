5339f3fb0b2dc0e85f592ede2c02444168009c28
Samuel Klein <96636730+SamuelJKlein@users.noreply.github.com>
2024-03-12 15:08:46 -0500

    Fixing problems with the flask part of the challenge

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -40,16 +40,16 @@ app = Flask(__name__)
 def home():
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start> <br/>"
-        f"/api/v1.0/<start>/<end> <br/>"
+        f"<a href='/api/v1.0/precipitation'>/api/v1.0/precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>/api/v1.0/stations</a><br/>"
+        f"<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br/>"
+        f"/api/v1.0/*insert start date*<br/>"
+        f"/api/v1.0/*insert start date*/*insert end date* <br/>"
     )
 # Precipitation Route returns precpitation data from the last year recorded in the database
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    prcp_data = session.query(Measurement.date,Measurement.tobs).filter(Measurement.date >= '2016-08-23').all()
+    prcp_data = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= '2016-08-23').all()
     prcp_dict = {}
     for row in prcp_data:
         prcp_dict[row[0]] = row[1]
@@ -59,26 +59,50 @@ def precipitation():
 # Stations route returns JSON list of all stations in the database.
 @app.route("/api/v1.0/stations")
 def stations():
-    station_list = session.query(Measurement.station).group_by(Measurement.station)
-
+    station_data = session.query(Measurement.station).group_by(Measurement.station)
+    station_list = []
+    for row in station_data:
+        station_list.append(row[0])
     return jsonify(station_list)
 
 # tobs route returns temp data from the most active station for the last year recorded in the database.
 @app.route("/api/v1.0/tobs")
 def tobs():
-    temp_data = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-23').all()
+    temp_data = session.query(Measurement.date,Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-23').all()
+    temp_dict = {}
+    for row in temp_data:
+        temp_dict[row[0]] = row[1]
+    return jsonify(temp_dict)
 
-    return jsonify(temp_data)
+#start route returns data that was recorded following the date in the url.
 
-#start route returns all data from the most active station that follows the date in the url.
-@app.route("/api/v1.0/<start>")
-def start_date(starting_at):
-    data = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= starting_at).filter(Measurement.station == "USC00519281").all()
+@app.route("/api/v1.0/<start_date>")
+def start_date(startdate):
+    data = session.query(Measurement.date,func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).group_by(Measurement.date).filter(Measurement.date >= start_date).all()
+    data_dict = {}
+    for row in data:
+        row_dict = {}
+        row_dict['min_temp'] = row[1]
+        row_dict['max_temp'] = row[2]
+        row_dict['avg_temp'] = row[3]
+       
+        data_dict[row[0]] = row_dict
+    return jsonify(data_dict)
+#start end date route returns data that was recorded within the range of dates in the url.
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def start_end_date(start_date,end_date):
+    data = session.query(Measurement.date,func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).group_by(Measurement.date).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    data_dict = {}
+    for row in data:
+        row_dict = {}
+        row_dict['min_temp'] = row[1]
+        row_dict['max_temp'] = row[2]
+        row_dict['avg_temp'] = row[3]
+       
+        data_dict[row[0]] = row_dict
+    return jsonify(data_dict)
 
-    return jsonify(data)
-#start end date route returns all data from the most active station that is within the range of dates in the url.
-@app.route("/api/v1.0/<start>/<end>")
-def start_end_date(starting_at,ending_at):
-    data = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= starting_at).filter(Measurement.date <= ending_at).filter(Measurement.station == "USC00519281").all()
-
-    return jsonify(data)
+if __name__ == "__main__":
+    app.run(debug=False)
+    
+    
